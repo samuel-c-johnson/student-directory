5140ece770f43890a5f1cab6c2bad07c6b892da2
Sam Johnson <samuel.c.johnson2@gmail.com>
2017-11-10 12:02:21 +0000

    Added student_count variable

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 #first we print the list of students
 
 puts "The students of Villians Academy"
@@ -15,8 +16,4 @@ puts "Joffrey Baratheon"
 puts "Norman Bates"
 
 #finally, we print the total number of students
-
-print "Overall, we have "
-#it's important that print() doesn't add new line characters
-print 11
-print " great students."
+puts "Overall, we have #{student_count} great students."
